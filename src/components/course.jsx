74162c597c133b5c6a0789bbeea74967a3088763
jenberu <jemberu0970@gmail.com>
2024-10-19 08:32:11 +0300

    country component added

---
+++ src/components/course.jsx
@@ -0,0 +1,15 @@
+
+
+
+const Country = (props) => {
+    return (
+        <div className="country-continier">
+            <img src="" alt="" />
+            <h3></h3> 
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+        </div>
+    );
+}
