05686c3b62d41c0f2e120d43fe8521787c16ebc9
jenberu <jemberu0970@gmail.com>
2025-10-22 16:29:06 +0300

    Remove homepage from package.json and update AboutMe component with new details

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,22 +1,22 @@
 import React from "react";
-import '../styles/about_style.css';
+import "../styles/about_style.css";
 
 const AboutMe = () => {
   return (
     <div className="about-me">
       <h3>Fullstack Developer</h3>
       <p>
-        I am a Django-React full-stack developer and a 5
-        <sup>th</sup>-year Software Engineering student at Bahir Dar University. 
-        I am currently working with Intertechhub and Safaricom Talent Cloud, where I 
-        apply my skills in web development to build innovative and impactful solutions. 
-        Over the years, I have developed and worked on a range of exciting projects, 
-        which you can explore further in the Projects section.
+        I am a Django–React full-stack developer and a lecturer at Bahir Dar
+        University, where I also completed my studies in Software Engineering. I
+        am passionate about teaching, research, and building real-world
+        solutions that create impact.
       </p>
       <p>
-        These projects showcase my passion for technology and problem-solving, 
-        as well as my ability to tackle complex challenges in both front-end and 
-        back-end development, alongside continuous academic learning.
+        Previously, I worked with Intertechhub and Safaricom Talent Cloud, where
+        I applied my skills in full-stack web development to design and build
+        innovative, scalable applications. Over the years, I have developed and
+        contributed to a variety of exciting projects that reflect my passion
+        for technology, problem-solving, and continuous learning
       </p>
     </div>
   );
